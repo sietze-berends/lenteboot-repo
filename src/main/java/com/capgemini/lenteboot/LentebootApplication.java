fd2d365261784e16084894aa1b161b2dbed9dffd
sietze-berends <46671318+sietze-berends@users.noreply.github.com>
2019-02-13 09:39:41 +0100

    Initial commit

---
+++ src/main/java/com/capgemini/lenteboot/LentebootApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.lenteboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LentebootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LentebootApplication.class, args);
+	}
+
+}
+
